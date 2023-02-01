8dd25778ee4fd59d1568632f352635bc8e8161cf
sfleta <sfleta@student.21-school.ru>
2023-02-02 02:24:55 +0700

    fixing copyright issues

--- src/sfleta_string.h
+++ src/sfleta_string.h
@@ -1,5 +1,5 @@
-#ifndef SRC_S21_STRING_H_
-#define SRC_S21_STRING_H_
+#ifndef SRC_sfleta_STRING_H_
+#define SRC_sfleta_STRING_H_
 #include <stdarg.h>
 #include <stdio.h>
 #include <math.h>
@@ -7,11 +7,11 @@
 #include <wchar.h>
 #include <locale.h>
 
-#ifndef s21_NULL
-#define s21_NULL ((void *)0)
+#ifndef sfleta_NULL
+#define sfleta_NULL ((void *)0)
 #endif
 
-typedef unsigned int s21_size_t;
+typedef unsigned int sfleta_size_t;
 
 #define allsymbol "cdieEfgGosuxXpn%"
 
@@ -21,48 +21,48 @@ struct Param {
     char length;
 };
 
-void *s21_memchr(const void *str, int c, s21_size_t n);
-void *s21_memcpy(void *dest, const void *src, s21_size_t n);
-void *s21_memmove(void *dest, const void *src, s21_size_t n);
-void *s21_memset(void *str, int c, s21_size_t n);
-char *s21_strcat(char *dest, const char *src);
-char *s21_strchr(const char *str, int c);
-int s21_strcmp(const char *str1, const char *str2);
-int s21_strncmp(const char *str1, const char *str2, s21_size_t n);
-char *s21_strcpy(char *dest, const char *src);
-s21_size_t s21_strcspn(const char *str1, const char *str2);
-char *s21_strerror(int errnum);
-s21_size_t s21_strlen(const char *str);
-char *s21_strpbrk(const char *str1, const char *str2);
-char *s21_strrchr(const char *str, int c);
-s21_size_t s21_strspn(const char *str1, const char *str2);
-char *s21_strstr(const char *haystack, const char *needle);
-char *s21_strtok(char *str, const char *delim);
-int s21_memcmp(const void *str1, const void *str2, s21_size_t n);
-char *s21_strncat(char *dest, const char *src, s21_size_t n);
-char *s21_strncpy(char *dest, const char *src, s21_size_t n);
-char *s21_strrchr(const char *str, int c);
-char *s21_strtok(char *s, const char *delim);
+void *sfleta_memchr(const void *str, int c, sfleta_size_t n);
+void *sfleta_memcpy(void *dest, const void *src, sfleta_size_t n);
+void *sfleta_memmove(void *dest, const void *src, sfleta_size_t n);
+void *sfleta_memset(void *str, int c, sfleta_size_t n);
+char *sfleta_strcat(char *dest, const char *src);
+char *sfleta_strchr(const char *str, int c);
+int sfleta_strcmp(const char *str1, const char *str2);
+int sfleta_strncmp(const char *str1, const char *str2, sfleta_size_t n);
+char *sfleta_strcpy(char *dest, const char *src);
+sfleta_size_t sfleta_strcspn(const char *str1, const char *str2);
+char *sfleta_strerror(int errnum);
+sfleta_size_t sfleta_strlen(const char *str);
+char *sfleta_strpbrk(const char *str1, const char *str2);
+char *sfleta_strrchr(const char *str, int c);
+sfleta_size_t sfleta_strspn(const char *str1, const char *str2);
+char *sfleta_strstr(const char *haystack, const char *needle);
+char *sfleta_strtok(char *str, const char *delim);
+int sfleta_memcmp(const void *str1, const void *str2, sfleta_size_t n);
+char *sfleta_strncat(char *dest, const char *src, sfleta_size_t n);
+char *sfleta_strncpy(char *dest, const char *src, sfleta_size_t n);
+char *sfleta_strrchr(const char *str, int c);
+char *sfleta_strtok(char *s, const char *delim);
 
-void *s21_to_upper(const char *str);
-void *s21_to_lower(const char *str);
-void *s21_insert(const char *src, const char *str, s21_size_t start_index);
-void *s21_trim(const char *src, const char *trim_chars);
+void *sfleta_to_upper(const char *str);
+void *sfleta_to_lower(const char *str);
+void *sfleta_insert(const char *src, const char *str, sfleta_size_t start_index);
+void *sfleta_trim(const char *src, const char *trim_chars);
 
-char *s21_itoa(int n, char *mass);
-char *s21_reverse(char *str);
+char *sfleta_itoa(int n, char *mass);
+char *sfleta_reverse(char *str);
 int signum(int VALUE);
 
-int s21_sprintf(char *str, const char *format, ...);
-void s21_parsing(char* symbol, struct Param* pars);
-void* s21_ReadSpecific(struct Param* spec_f, va_list* argc, char* str);
-char* s21_WriteFlags(char* BUF, struct Param* mass_f, int* index, s21_size_t* LEN);
-char* s21_sprintf_d(char* BUF, struct Param* mass_f, int* index, s21_size_t* LEN);
-void s21_ClearFlag(struct Param* flag_c);
-char* s21_ditoa(unsigned long long int n, char *TMP);
-void* s21_FDot(char* TMP, struct Param* float_t, long double conv_f);
-char* s21_accuracy_s(char *TMP, int len);
-void s21_sprintf_s(char* BUF, struct Param* mass_f, int* index, s21_size_t* LEN);
-void s21_sprintfRigth(char* BUF, struct Param* mass_f, int* index, s21_size_t* LEN);
+int sfleta_sprintf(char *str, const char *format, ...);
+void sfleta_parsing(char* symbol, struct Param* pars);
+void* sfleta_ReadSpecific(struct Param* spec_f, va_list* argc, char* str);
+char* sfleta_WriteFlags(char* BUF, struct Param* mass_f, int* index, sfleta_size_t* LEN);
+char* sfleta_sprintf_d(char* BUF, struct Param* mass_f, int* index, sfleta_size_t* LEN);
+void sfleta_ClearFlag(struct Param* flag_c);
+char* sfleta_ditoa(unsigned long long int n, char *TMP);
+void* sfleta_FDot(char* TMP, struct Param* float_t, long double conv_f);
+char* sfleta_accuracy_s(char *TMP, int len);
+void sfleta_sprintf_s(char* BUF, struct Param* mass_f, int* index, sfleta_size_t* LEN);
+void sfleta_sprintfRigth(char* BUF, struct Param* mass_f, int* index, sfleta_size_t* LEN);
 
-#endif  // SRC_S21_STRING_H_
+#endif  // SRC_sfleta_STRING_H_
